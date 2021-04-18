408e917f7d964efcfedd8321ecb3746443e38ad6
Fatality <fatihcayir2001@gmail.com>
2021-04-19 01:11:22 +0300

    Update 1

--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navi',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NaviComponent implements OnInit {
 
-  constructor() { }
+  user:User 
+  userId:number;
+  isAuthenticated:boolean
+  
+  constructor(
+    private authService:AuthService,
+    private toastrService:ToastrService,
+    private router:Router,
+    private userService:UserService
+  ) { }
 
   ngOnInit(): void {
+
+
+    if (this.checkIfLogin()) {
+      this.getUserByEmail();
+      console.log("True")
+      this.getUserId();
+    
+    }
+  }
+
+
+  checkIfLogin(){
+    if (this.authService.isAuthenticated()) {
+      return true
+    }else{
+      return false
+    }
+  }
+
+  getUserId(){
+    this.userId =  this.userService.getUserId()
+   }
+ 
+   getUserByEmail() {
+     console.log("Çalıştı ")
+     this.userService.getByMail(localStorage.getItem("email")).subscribe((response) => {
+       this.user = response.data;
+       console.log(this.user)
+     });
+   }
+  
+
+   logOut(){
+    this.authService.logOut();
+    this.toastrService.info("Başarılı  Çıkış Yapıldı")
+    this.router.navigate([""])    
   }
 
 }
